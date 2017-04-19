6248af8e93b9e06f8b7b0f0388b775799d2aa415
sirdir <phddir@gmail.com>
2017-04-19 14:44:33 +0300

    docker ready for actionn

--- conf.js
+++ conf.js
@@ -1,12 +1,23 @@
 let Jasmine2HtmlReporter = require('wordwrap-protractor-jasmine2-html-reporter');
 let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 let curDate = Date.now();
+let shell = require('shelljs');
 
 
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./specs/chat.js'],
+    beforeLaunch: function () {
+        shell.exec('docker pull selenium/standalone-chrome:3.3.1-cesium', {silent: true});
+        shell.exec('docker stop $(docker ps -aq -f "ancestor=selenium/standalone-chrome:3.3.1-cesium")', {silent: true});
+        shell.exec('docker rm $(docker ps -aq -f "name=macpaw_docker_chrome")', {silent: true});
+        shell.exec('docker run --name macpaw_docker_chrome -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:3.3.1-cesium', {silent: true});
+        shell.exec('docker logs $(docker ps -aq -f "name=macpaw_docker_chrome")', {silent: true});
+    },
+    afterLaunch: () => {
+        shell.exec('docker stop $(docker ps -aq -f "ancestor=selenium/standalone-chrome:3.3.1-cesium")', {silent: true});
+    },
     onPrepare: function(){
         global.dv = browser.driver;
         global.EC = protractor.ExpectedConditions;
